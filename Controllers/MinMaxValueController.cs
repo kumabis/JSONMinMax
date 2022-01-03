5f1503f097ee849713898102be47f75fa4f0e70c
BISHAL KUMAR <kumar.bishal1996@outlook.com>
2022-01-04 03:55:12 +0530

    Add project files.

---
+++ Controllers/MinMaxValueController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace JSONMinMax.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class MinMaxValueController : ControllerBase
+    {
+        public MinMaxValueController()
+        {
+        }
+
+        [HttpGet]
+        public string GetMinMaxValue()
+        {
+            var employeeList = Download_serialized_json_data<Employees>();
+            List<int> idList = employeeList.Select(a => Convert.ToInt32(a.id)).ToList();
+            int minId = idList[0], maxId = idList[0];
+            for(int i = 1; i < idList.Count(); i++)
+            {
+                if (idList[i] >= maxId)
+                {
+                    maxId = idList[i];
+                }
+                if (idList[i] <= minId)
+                {
+                    minId = idList[i];
+                }
+            }
+            return string.Concat("Minimum Id : " + minId, "\n", "Maximum Id : " + maxId);
+        }
+        private static List<Employee> Download_serialized_json_data<T>() where T : new()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Headers.Add("user-agent", "Only a test!");
+                string json = webClient.DownloadString("https://api.github.com/users/hadley/orgs");
+                return !string.IsNullOrEmpty(json) ? JsonConvert.DeserializeObject<List<Employee>>(json) : new List<Employee>();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace JSONMinMax
+{
+    public class Employee
+    {
+        public string login { get; set; }
+        public string id { get; set; }
+        public string node_id { get; set; }
+        public string url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string hooks_url { get; set; }
+        public string issues_url { get; set; }
+        public string members_url { get; set; }
+        public string public_members_url { get; set; }
+        public string description { get; set; }
+    }
+}
