4e78ebbb63a3dc6c694d23187250219ea1f39d66
BISHAL KUMAR <kumar.bishal1996@outlook.com>
2022-01-04 11:57:03 +0530

    dependency injection added in startup file

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using JSONMinMax.IServices;
+using JSONMinMax.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +21,7 @@ namespace JSONMinMax
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IResultService, ResultService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
